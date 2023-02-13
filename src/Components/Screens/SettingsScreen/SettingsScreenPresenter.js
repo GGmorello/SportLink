38820e9187341f7f132c390fac93fa9b4d619a8e
Gabriele Morello <55058684+GGmorello@users.noreply.github.com>
2023-02-13 15:37:21 +0100

    initial settinga

--- src/Components/Screens/SettingsScreen/SettingsScreenPresenter.js
+++ src/Components/Screens/SettingsScreen/SettingsScreenPresenter.js
@@ -1,6 +1,77 @@
-import React from "react";
-import SettingsScreenView from "./SettingsScreenView";
+import React, { useState } from 'react';
+import SettingsScreenView from './SettingsScreenView';
 
-export default function SettingsScreenPresenter() {
-  return (<SettingsScreenView />)
-}
+const SettingsScreenPresenter = () => {
+  const [matchingSports, setMatchingSports] = useState(true);
+  const [allSports, setAllSports] = useState(false);
+  const [customSports, setCustomSports] = useState(false);
+  const [customSportsInput, setCustomSportsInput] = useState('');
+  const [distance, setDistance] = useState(0);
+  const [ageInterval, setAgeInterval] = useState(0);
+  const [gender, setGender] = useState('all');
+
+  const handleMatchingSportsPress = () => {
+    setMatchingSports(true);
+    setAllSports(false);
+    setCustomSports(false);
+  };
+
+  const handleAllSportsPress = () => {
+    setMatchingSports(false);
+    setAllSports(true);
+    setCustomSports(false);
+  };
+
+  const handleCustomSportsPress = () => {
+    setMatchingSports(false);
+    setAllSports(false);
+    setCustomSports(true);
+  };
+
+  const handleCustomSportsInputChange = (text) => {
+    setCustomSportsInput(text);
+  };
+
+  const handleDistanceValueChange = (value) => {
+    setDistance(value);
+  };
+
+  const handleAgeIntervalValueChange = (value) => {
+    setAgeInterval(value);
+  };
+
+  const handleMenPress = () => {
+    setGender('men');
+  };
+
+  const handleWomenPress = () => {
+    setGender('women');
+  };
+
+  const handleAllPress = () => {
+    setGender('all');
+  };
+
+  return (
+    <SettingsScreenView
+      matchingSports={matchingSports}
+      allSports={allSports}
+      customSports={customSports}
+      customSportsInput={customSportsInput}
+      distance={distance}
+      ageInterval={ageInterval}
+      gender={gender}
+      handleMatchingSportsPress={handleMatchingSportsPress}
+      handleAllSportsPress={handleAllSportsPress}
+      handleCustomSportsPress={handleCustomSportsPress}
+      handleCustomSportsInputChange={handleCustomSportsInputChange}
+      handleDistanceValueChange={handleDistanceValueChange}
+      handleAgeIntervalValueChange={handleAgeIntervalValueChange}
+      handleMenPress={handleMenPress}
+      handleWomenPress={handleWomenPress}
+      handleAllPress={handleAllPress}
+    />
+  );
+};
+
+export default SettingsScreenPresenter;
--- src/Components/Screens/SettingsScreen/SettingsScreenView.js
+++ src/Components/Screens/SettingsScreen/SettingsScreenView.js
@@ -1,9 +1,77 @@
-import React from "react";
-import {View, Text} from "react-native"
+import React from 'react';
+import { View, Text, TouchableOpacity, TextInput, Slider } from 'react-native';
 
-export default function SettingsScreenView(props) {
-    return (
-        <View>
-            <Text>SettingsScreen</Text>
-        </View>)
-}
+const SettingsScreenView = ({
+  matchingSports,
+  allSports,
+  customSports,
+  customSportsInput,
+  distance,
+  ageInterval,
+  gender,
+  handleMatchingSportsPress,
+  handleAllSportsPress,
+  handleCustomSportsPress,
+  handleCustomSportsInputChange,
+  handleDistanceValueChange,
+  handleAgeIntervalValueChange,
+  handleMenPress,
+  handleWomenPress,
+  handleAllPress,
+}) => {
+  return (
+    <View>
+      <Text>Settings</Text>
+      <View>
+        <Text>Sports Selection:</Text>
+        <TouchableOpacity onPress={handleMatchingSportsPress}>
+          <Text>Matching my sports</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handleAllSportsPress}>
+          <Text>All sports</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handleCustomSportsPress}>
+          <Text>Custom</Text>
+        </TouchableOpacity>
+        {customSports && (
+          <View>
+            <TextInput
+              value={customSportsInput}
+              onChangeText={handleCustomSportsInputChange}
+            />
+          </View>
+        )}
+      </View>
+      <View>
+        <Text>Distance: {distance} km</Text>
+        <Slider
+          value={distance}
+          onValueChange={handleDistanceValueChange}
+          minimumValue={0}
+          maximumValue={150}
+        />
+      </View>
+      <View>
+        <Text>Age Interval: {ageInterval}</Text>
+        <Slider
+          value={ageInterval}
+          onValueChange={handleAgeIntervalValueChange}
+        />
+      </View>
+      <View>
+        <Text>Gender:</Text>
+        <TouchableOpacity onPress={handleMenPress}>
+          <Text>Men</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handleWomenPress}>
+          <Text>Women</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handleAllPress}>
+          <Text>All</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default SettingsScreenView;
