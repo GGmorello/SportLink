55bc9aa81a27372e9afadcc0d685b77823b0bb60
Adrelix <adreli99@gmail.com>
2023-02-13 15:01:57 +0100

    fixed indentation

--- src/Components/Screens/ChatScreen/ChatScreenView.js
+++ src/Components/Screens/ChatScreen/ChatScreenView.js
@@ -35,54 +35,54 @@ export default function ChatScreenView(props) {
   }
 
   return (
-      <View style={styles.container}>
-      <View style={styles.chatContainer}>
-        <FlatList 
-          style={styles.list}
-          data={messageHistory}
-          ref={flatlistRef}
-          keyExtractor= {(item) => {
-            return item.id;
-          }}
-          renderItem={(message) => {
-            const item = message.item;
-            let inMessage = item.type === 'in';
-            let itemStyle = inMessage ? styles.itemIn : styles.itemOut;
-            let timeStamp = inMessage ? styles.timeStampIn : styles.timeStampOut;
+        <View style={styles.container}>
+        <View style={styles.chatContainer}>
+            <FlatList 
+            style={styles.list}
+            data={messageHistory}
+            ref={flatlistRef}
+            keyExtractor= {(item) => {
+                return item.id;
+            }}
+            renderItem={(message) => {
+                const item = message.item;
+                let inMessage = item.type === 'in';
+                let itemStyle = inMessage ? styles.itemIn : styles.itemOut;
+                let timeStamp = inMessage ? styles.timeStampIn : styles.timeStampOut;
 
-            return (
-              <View>
-                <View style={timeStamp}>
-                  <Text>{renderDate(item.date)}</Text>
+                return (
+                <View>
+                    <View style={timeStamp}>
+                    <Text>{renderDate(item.date)}</Text>
+                    </View>
+                    
+                <View style={[styles.item, itemStyle]}>
+                    {!inMessage}
+                    <View style={[styles.balloon]}>
+                    <Text>{item.message}</Text>
+                    </View>
+                    {inMessage}
                 </View>
-                
-              <View style={[styles.item, itemStyle]}>
-                {!inMessage}
-                <View style={[styles.balloon]}>
-                  <Text>{item.message}</Text>
                 </View>
-                {inMessage}
-              </View>
-              </View>
-            )
-          }}
-        />
-      </View>
-      <View style={styles.footer}>
-        <View style={styles.inputContainer}>
-          <TextInput style={styles.inputs}
-              placeholder="Write a message..."
-              underlineColorAndroid='transparent'
-              value={props.currentMessage}
-              autoFocus={true}
-              onSubmitEditing={sendMessage}
-              onChangeText={(text) => setMessage(text)}/>
+                )
+            }}
+            />
         </View>
-        <TouchableOpacity style={styles.btnSend}  onPress={() => {sendMessage()}}>
-          <Image source={{uri:"https://img.icons8.com/small/75/ffffff/filled-sent.png"}}  style={styles.iconSend}  />
-        </TouchableOpacity>
-      </View>
-    </View>
+        <View style={styles.footer}>
+            <View style={styles.inputContainer}>
+            <TextInput style={styles.inputs}
+                placeholder="Write a message..."
+                underlineColorAndroid='transparent'
+                value={props.currentMessage}
+                autoFocus={true}
+                onSubmitEditing={sendMessage}
+                onChangeText={(text) => setMessage(text)}/>
+            </View>
+            <TouchableOpacity style={styles.btnSend}  onPress={() => {sendMessage()}}>
+            <Image source={{uri:"https://img.icons8.com/small/75/ffffff/filled-sent.png"}}  style={styles.iconSend}  />
+            </TouchableOpacity>
+        </View>
+        </View>
     );
 
 }
