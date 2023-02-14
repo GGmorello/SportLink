e2a8a864849a0b841b0b55b7b617a220ad630bf9
Gard Aasness <gardaasness@gmail.com>
2023-02-14 19:42:48 +0100

    Add text and buttons with styling

--- src/Components/Screens/SwipeScreen/SwipeScreenStyle.js
+++ src/Components/Screens/SwipeScreen/SwipeScreenStyle.js
@@ -3,25 +3,82 @@ import { StyleSheet } from "react-native"
 
 const styles = StyleSheet.create({
     image: {
-        width: 180,
-        height: 180,
-        overflow: 'hidden',
-        marginBottom: 10,
-      },
-      navigationBar:{
-        position: "absolute",
-        width: "100%",
-        bottom: 0,
+      width: 180,
+      height: 180,
+      overflow: 'hidden',
+      marginBottom: 10,
+      marginRight: 10,
+    },
+    navigationBar:{
+      position: "absolute",
+      width: "100%",
+      bottom: 0,
     },
     screen: {
       flex: 1,           
     },
-    verticalScrollView:{
+    screenLayout:{
       marginHorizontal: 10,
-      marginBottom:75,
+      marginBottom: 75,
+      marginVertical: 10,
+    },
+    horizontalScrollView: {
+        marginVertical: 5,
+    },
+    rowContainer: {
+      flexDirection: 'row',
+    },
+    rowContainerSpaceBetween: {
+      flexDirection: 'row',
+      justifyContent: "space-between",
+      marginHorizontal: 20,
+      marginVertical: 20,
+    },
+    name: {
+      fontSize: 30
+    },
+    age: {
+      fontSize: 22
+    },
+    distance: {
+      fontSize: 15
+    },
+    sectionTitle: {
+      color: "#3f3f3f",
+      marginBottom: 10,
+    },
+    containerIcon: {
+      flexDirection: 'row', 
+      backgroundColor: '#B4CBF0',
+      borderColor:'#B4CBF0',
+      borderWidth: 1,
+      marginRight: 10,
+      marginBottom: 10,
+      paddingTop: 5,
+      paddingBottom: 5,
+      paddingRight: 10,
+      paddingLeft: 10,
+      borderRadius: 10
   },
-  horizontalScrollView: {
-      marginVertical: 5,
+  approveButton: {
+    width: 65,
+    height: 65,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+    borderRadius: 100,
+    backgroundColor: '#2CF10C',
+    opacity: 0.7,
+  },
+  rejectButton: {
+    width: 65,
+    height: 65,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+    borderRadius: 100,
+    backgroundColor: '#F20B0B',
+    opacity: 0.7,
   },
 });
 
--- src/Components/Screens/SwipeScreen/SwipeScreenView.js
+++ src/Components/Screens/SwipeScreen/SwipeScreenView.js
@@ -1,20 +1,57 @@
 import React from "react";
-import {View, Text, Image} from "react-native";
+import {View, Text, Image, TouchableOpacity} from "react-native";
 import styles from "./SwipeScreenStyle";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 export default function SwipeScreenView(props) {
 
     let erik = {
-
+        "name": "Erik",
+        "age": 28,
+        "distance": 5,
+        "images": [],
+        "interests": ["Padel"],
+        "biography": "Im an extrovert who enjoys doing sports with other people. I wish to find a running partner so we can motivate each other, and a padel partner to team with for an upcoming tournament (medium level). My achievements are: together with my partner, we placed second in a local padel tournament here in Stockholm. In regards to running, I have ran multiple marathon, and my personal best time is 3 hours and 37 minutes."
     }
 
     let mick = {
-
+        "name": "Mick",
+        "age": 28,
+        "distance": 10,
+        "images": [],
+        "interests": ["Padel, Jogging"],
+        "biography": "Im an extrovert who enjoys doing sports with other people. I wish to find a running partner so we can motivate each other, and a padel partner to team with for an upcoming tournament (medium level). My achievements are: together with my partner, we placed second in a local padel tournament here in Stockholm. In regards to running, I have ran multiple marathon, and my personal best time is 3 hours and 37 minutes."
     }
 
     return (
-        <View style={styles.verticalScrollView}>
-            <Image source={require('./images/erik1.jpg')} style={styles.image}/>
-
+        <View style={styles.screenLayout}>
+            <View style={styles.rowContainer}>
+                <Image source={require('./images/erik1.jpg')} style={styles.image}/>
+                <View>
+                    <Text style={styles.name}>{erik.name}</Text>
+                    <Text style={styles.age}>{erik.age}</Text>
+                    <Text style={styles.distance}>{erik.distance}km away</Text>
+                </View>
+            </View>
+            <Text style={styles.sectionTitle}>
+                Biography
+            </Text>
+            <Text>
+                {erik.biography}
+            </Text>
+            <View style={styles.rowContainerSpaceBetween}>
+                <TouchableOpacity
+                    onPress={null}
+                    style={styles.rejectButton}
+                >
+                    <FontAwesome name="close" color="#FFFFFF" size={35} />
+                </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={null}
+                    style={styles.approveButton}
+                >
+                    <FontAwesome name="check" color="#FFFFFF" size={35}/>
+                </TouchableOpacity>
+            </View>
         </View>)
 }
