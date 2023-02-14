19564d1e2d939c5c7f57f9bdf9eb2550206ac969
Adrelix <adreli99@gmail.com>
2023-02-14 15:25:49 +0100

    removed navigation animations

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -15,7 +14,7 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={{ animation: 'none' }}>
         <Stack.Screen
           name="SwipeScreen"
           component={SwipeScreenPresenter}
