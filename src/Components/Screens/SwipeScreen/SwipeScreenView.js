42287453899556ef20a39895924cd22365d178be
Gard Aasness <gardaasness@gmail.com>
2023-02-15 11:58:44 +0100

    Add useState to switch between users

--- src/Components/Screens/SwipeScreen/SwipeScreenView.js
+++ src/Components/Screens/SwipeScreen/SwipeScreenView.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {View, Text, Image, TouchableOpacity} from "react-native";
 import styles from "./SwipeScreenStyle";
 import FontAwesome from '@expo/vector-icons/FontAwesome';
@@ -10,7 +10,7 @@ export default function SwipeScreenView(props) {
         "name": "Erik",
         "age": 28,
         "distance": 5,
-        "images": [],
+        "images": ['./images/erik1.jpg', './images/erik2.jpg', './images/erik3.jpg'],
         "interests": ["Padel"],
         "biography": "Im an extrovert who enjoys doing sports with other people. I wish to find a running partner so we can motivate each other, and a padel partner to team with for an upcoming tournament (medium level). My achievements are: together with my partner, we placed second in a local padel tournament here in Stockholm. In regards to running, I have ran multiple marathon, and my personal best time is 3 hours and 37 minutes."
     }
@@ -19,10 +19,16 @@ export default function SwipeScreenView(props) {
         "name": "Mick",
         "age": 28,
         "distance": 10,
-        "images": [],
+        "images": ['./images/mick1.jpg', './images/mick2.jpg', './images/mick3.jpg'],
         "interests": ["Padel, Jogging"],
         "biography": "Im an extrovert who enjoys doing sports with other people. I wish to find a running partner so we can motivate each other, and a padel partner to team with for an upcoming tournament (medium level). My achievements are: together with my partner, we placed second in a local padel tournament here in Stockholm. In regards to running, I have ran multiple marathon, and my personal best time is 3 hours and 37 minutes."
     }
+
+    function onButtonClick() {
+        setPerson(mick);
+    }
+
+    const [person, setPerson] = useState(erik);
 
     return (
         <View style={styles.screenLayout}>
@@ -34,9 +40,9 @@ export default function SwipeScreenView(props) {
                     </View>
                 </ImageBackground>
                 <View>
-                    <Text style={styles.name}>{erik.name}</Text>
-                    <Text style={styles.age}>{erik.age}</Text>
-                    <Text style={styles.distance}>{erik.distance}km away</Text>
+                    <Text style={styles.name}>{person.name}</Text>
+                    <Text style={styles.age}>{person.age}</Text>
+                    <Text style={styles.distance}>{person.distance}km away</Text>
                 </View>
             </View>
             <Text style={styles.sectionTitle}>
@@ -49,17 +55,17 @@ export default function SwipeScreenView(props) {
                 Biography
             </Text>
             <Text>
-                {erik.biography}
+                {person.biography}
             </Text>
             <View style={{...styles.rowContainer, justifyContent:"space-between", marginHorizontal: 20, marginVertical: 20}}>
                 <TouchableOpacity
-                    onPress={null}
+                    onPress={() => onButtonClick()}
                     style={styles.rejectButton}
                 >
                     <FontAwesome name="close" color="#FFFFFF" size={35} />
                 </TouchableOpacity>
                 <TouchableOpacity
-                    onPress={null}
+                    onPress={() => onButtonClick()}
                     style={styles.approveButton}
                 >
                     <FontAwesome name="check" color="#FFFFFF" size={35}/>
